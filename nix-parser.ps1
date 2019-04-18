4b5fc5c018ed47021effcc71899845353159b99d
Aleksandr Mogilnikov <zorion79@gmail.com>
2019-04-18 15:32:11 +1000

    init commit

---
+++ nix-parser.ps1
@@ -0,0 +1,24 @@
+Install-Module ImportExcel -Scope CurrentUser
+Import-Module ImportExcel
+
+$urls = Import-Excel .\urls.xlsx
+# $url = "https://www.nix.ru/autocatalog/server_systems_intel/Intel-2U-R2208WT2YSR-LGA2011-3-C612-2xPCI-E-SVGA-SATA-RAID-8xHotSwapSAS-SATA-2xGbLAN-24DDR4-1100W_280008.html"
+
+$result = @()
+foreach ($url in $urls) {
+    $webResponse = Invoke-WebRequest $url.url
+
+    $price = (($webResponse.ParsedHtml.getElementsByTagName("table") | Where-Object { $_.id -eq "goods_buttons" }).getElementsByTagName("span"))[0].textContent
+    $name = ((($webResponse.ParsedHtml.getElementsByTagName("div") | `
+                    Where-Object { $_.id -eq "goods_center" }).getElementsByTagName("span") | `
+                Where-Object { $_.id -eq "goods_name" }).getElementsByTagName("span"))[0].textContent
+    
+    $o = [PSCustomObject]@{
+        url   = $url.P1
+        price = $price
+        name  = $name
+    }
+    $result += $o
+}
+
+$result | Export-Excel .\urls.xlsx -AutoSize -Show
